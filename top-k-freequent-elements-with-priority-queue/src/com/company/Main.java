b7b3fc66c3b0729959ad84d8e36e0625ac4abbfd
Yu Lin <yulin@Yus-MacBook-Air.local>
2020-06-02 11:38:43 -0400

    working on top-k-freequent-elements-with-priority-queue

---
+++ top-k-freequent-elements-with-priority-queue/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+
+public class Main {
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+}
