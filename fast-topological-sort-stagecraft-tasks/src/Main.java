225d64fea939841018f0c6bf9b8df84d66d022d9
Yu Lin <linyu3924@gmail.com>
2020-11-13 18:24:53 -0500

    got fast topological order to work... still need to work on timing for each task

--- fast-topological-sort-stagecraft-tasks/src/Main.java
+++ fast-topological-sort-stagecraft-tasks/src/Main.java
@@ -4,7 +4,9 @@ import java.util.stream.Collectors;
 
 public class Main {
   private static int i = 0;
+  private static List<Integer> tasksT;
 
+  /** @param args */
   public static void main(String[] args) {
     // write your code here
     Scanner scanner = new Scanner((System.in));
@@ -14,8 +16,9 @@ public class Main {
     //      input.add(scanner.nextLine());
     //    }
     //    scanner.close();
-//    input.addAll(Arrays.asList("4", "2 4", "-1", "-1", "3", "10 11 8 5"));
-    input.addAll(Arrays.asList("7", "2 3 4", "4 5", "6", "3 6", "4", "-1", "4 5 6", "10 15 20 31 5 14 2"));
+    //    input.addAll(Arrays.asList("4", "2 4", "-1", "-1", "3", "10 11 8 5"));
+    input.addAll(
+        Arrays.asList("7", "2 3 4", "4 5", "6", "3 6", "4", "-1", "4 5 6", "10 15 20 31 5 14 2"));
     // Testcase 1 expected results
     //    10
     //    21
@@ -24,7 +27,7 @@ public class Main {
     Integer totalTasks = Integer.parseInt(input.get(0));
 
     // storing time requirement of each task in a List <Integer>
-    List<Integer> tasksT =
+    tasksT =
         Arrays.stream(input.get(input.size() - 1).split(" "))
             .map(x -> Integer.parseInt(x.split(" ")[0]))
             .collect(Collectors.toList());
@@ -54,66 +57,120 @@ public class Main {
     fastTopologicalSort(totalTasks, edges);
   }
 
-
-
-
+  /**
+   * This method sorts a DAG using fast topological sort
+   * Calls deleteTask() recursively
+   * @param totalTasks
+   * @param edges
+   */
   private static void fastTopologicalSort(Integer totalTasks, List<Integer[]> edges) {
+
+    // Mark each node with In-Degree
     Queue<Integer> queue = new LinkedList<>();
-    Integer inNeighbourCount[] = new Integer[totalTasks];
+    Integer[] inDegree = new Integer[totalTasks];
+    Map<Integer, List<Integer>> inNeighbours = new HashMap<Integer, List<Integer>>();
     for (int j = 0; j < totalTasks; j++) {
-      inNeighbourCount[j] = 0;
+      inDegree[j] = 0;
     }
     for (int j = 0; j < totalTasks; j++) {
-      Integer[] edge = edges.get(j);
-      for (int k = 0; k < edge.length; k++) {
-        if (edge[0] != -1) {
-          inNeighbourCount[edge[k] - 1]++;
+      Integer[] otherNode = edges.get(j);
+      for (int k = 0; k < otherNode.length; k++) {
+        if (otherNode[0] != -1) {
+          inDegree[otherNode[k] - 1]++;
+          List<Integer> list = inNeighbours.getOrDefault(otherNode[k] - 1, new ArrayList<>());
+          list.add(j);
+          inNeighbours.put(otherNode[k] - 1, list);
         }
       }
     }
-    for (int j = 0; j < inNeighbourCount.length; j++) {
-      if (inNeighbourCount[j] == 0) queue.add(j+1);
+
+    // Add all 0-In-Degree nodes to the queue
+    for (int j = 0; j < inDegree.length; j++) {
+      if (inDegree[j] == 0) queue.add(j);
     }
 
     System.out.println("\nIn-Degree of each task:");
-    Arrays.asList(inNeighbourCount).forEach(System.out::println);
+    Arrays.asList(inDegree).forEach(System.out::println);
+    System.out.println("\nIn-Neighbours of each task:");
+    for (int j = 0; j < totalTasks; j++) {
+      System.out.println(inNeighbours.getOrDefault(j, Arrays.asList(new Integer[1])));
+    }
     System.out.print("\nCurrent queue : ");
     queue.forEach(x -> System.out.print(x + " "));
-    System.out.println("\n\nStarting fast TopologicalSort () --------------\n");
 
 
+    // While Queue is not empty, recursively delete nodes
+    Integer[] topOrder = new Integer[totalTasks];
     while (!queue.isEmpty()) {
-      i++;
+      System.out.println("\n\nStarting fastTopologicalSort () --------------\n");
       int taskID = queue.poll();
-      System.out.println("\nCurrent taskID : " + taskID + " in " + i + " place" );
+      queue.remove(taskID);
       System.out.println("\n\nRecursive deleteTask()");
-      deleteTask(queue, edges, taskID, inNeighbourCount);
-
+      deleteTask(edges, taskID, inDegree, inNeighbours,topOrder);
     }
-    System.out.println("\n\nEnding fast TopologicalSort () --------------");
+    System.out.println("\n\nEnding fastTopologicalSort () --------------");
+
+
+
+
+    Arrays.asList(topOrder).stream().forEach(x->System.out.println(x));
   }
 
+  public void calcTaskTimings (Integer totalTasks, Integer [] topOrder) {
 
 
+    Integer[] totalTimes = new Integer[totalTasks];
+    Arrays.fill(totalTimes, 42);
+    for (int j =0; j<topOrder.length; j++){
+      Integer task = topOrder[j];
+
+    }
+
+  }
+
+  /**
+   * This is a private and recursive method used in topological sort.
+   * @param edges
+   * @param taskID
+   * @param inDegree
+   */
   private static void deleteTask(
-      Queue<Integer> queue, List<Integer[]> edges, int taskID, Integer[] inNeighbours) {
+      List<Integer[]> edges,
+      int taskID,
+      Integer[] inDegree,
+      Map<Integer, List<Integer>> inNeighbours,
+      Integer[] topOrder
+  ) {
+
+    // Stop Recursing once all nodes have been counted using iterator i
+    if (i == inDegree.length) {
+      return;
+    }
+
     i++;
-    System.out.println("\nCurrent taskID : " + (taskID +1) + " in " + i + " place" );
+    System.out.println("\nCurrent taskID : " + (taskID + 1) + " in " + i + " place");
+    Integer[] outNeigbours = new Integer[edges.get(taskID).length];
+    if (i>0)topOrder[i-1] = taskID + 1;
 
-    for (int j = 0; j < edges.get(taskID).length; j++) {
+    // Find all out-neighbours of current node
+    outNeigbours = edges.get(taskID);
+    if (outNeigbours[0] == -1) {
+      System.out.println("In-Degree of each task:");
+      Arrays.asList(inDegree).forEach(System.out::println);
+      return;
+    }
 
-      Integer[] outNeigbours = edges.get(taskID);
-      if (outNeigbours[0] == -1){
-        break;
-      }
-      for (int k = 0; k < outNeigbours.length; k++) {
-        inNeighbours[outNeigbours[k]-1]--;
-      }
-      for (int k = 0; k < outNeigbours.length; k++) {
-        if (inNeighbours[outNeigbours[k]-1] == 0) {
-          taskID = outNeigbours[k]-1;
-          deleteTask(queue, edges, taskID, inNeighbours);
-        }
+    // Subtract 1 from all out-neighbours' in-degree
+    for (int k = 0; k < outNeigbours.length; k++) {
+      inDegree[outNeigbours[k] - 1]--;
+    }
+    System.out.println("In-Degree of each task:");
+    Arrays.asList(inDegree).forEach(System.out::println);
+
+    for (int k = 0; k < outNeigbours.length; k++) {
+      if (inDegree[outNeigbours[k] - 1] == 0) {
+        taskID = outNeigbours[k] - 1;
+        deleteTask(edges, taskID, inDegree, inNeighbours, topOrder);
       }
     }
   }
