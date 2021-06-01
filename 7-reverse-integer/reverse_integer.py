7dcfd93bb02e0644ecca32e2e9dad87bb96811de
Yu Lin <gremlinweekend@yus-macbook-air.myfiosgateway.com>
2021-05-31 20:39:22 -0400

    completed problem 8

--- 7-reverse-integer/reverse_integer.py
+++ 7-reverse-integer/reverse_integer.py
@@ -2,6 +2,41 @@
 '''
 7. Reverse Integer
 
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1],
+then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+Example 4:
+
+Input: x = 0
+Output: 0
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+
+Success!
+
+Runtime: 24 ms, faster than 37.37% of Python online submissions for Reverse Integer.
+Memory Usage: 13.6 MB, less than 9.52% of Python online submissions for Reverse Integer.
+
 '''
 
 
@@ -24,6 +59,9 @@ class Solution(object):
         if new_lst[0] == 0: new_lst = new_lst[1:]
         reversed_int = int(''.join(new_lst))
         reversed_int = -(reversed_int) if prefix == 'negative' else reversed_int
+
+        # prevents the calculation to go outside of signed int range
+        if reversed_int > 2147483647 or reversed_int < -2147483647 : return 0
         return reversed_int
         
 
